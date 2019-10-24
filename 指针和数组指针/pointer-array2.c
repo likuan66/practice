4b4a772ad516440e9ee89493c8b3fc0756920eab
likuan66 <1402424218@qq.com>
2019-10-24 22:21:21 +0800

    practice

---
+++ 指针和数组指针/pointer-array2.c
@@ -0,0 +1,26 @@
+//指针与二维数组
+#include<stdio.h>
+#include<Windows.h>
+
+int main()
+{
+	int array[4][5] = { 0 };
+	int i, j, k=0;
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			array[i][j] = k++;
+		}
+	}
+	printf("*(array+1):%p\n", *(array + 1));
+	printf("array[1]:%p\n", array[1]);
+	printf("&array[1][0]:%p\n", &array[1][0]);
+	printf("**(array+1):%d\n", **(array + 1));
+	printf("*(*(array+1)+3):%d\n", *(*(array + 1) + 3));
+	printf("array[1][3]:%d\n", array[1][3]);
+
+	//总结：*(array+i)=array[i]  *(*(array+i)+j)=array[i][j]    *(*(*(array+i)+j)+k)=array[i][j][k]
+	system("pause");
+	return 0;
+}
