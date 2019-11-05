01a50c4c4f4ae851d1732b957f5e517bcf2129b8
likuan66 <1402424218@qq.com>
2019-11-05 13:13:40 +0800

    practice

---
+++ 小甲鱼经典例题/程序6.c
@@ -0,0 +1,15 @@
+//题目：用*号输出字母C的图案。
+#include<stdio.h>
+#include<Windows.h>
+
+int main()
+{
+	printf("*************\n");
+	printf("**\n");
+	printf("**\n");
+	printf("**\n");
+	printf("**\n");
+	printf("*************\n");
+	system("pause");
+	return 0;
+}
